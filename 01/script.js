8fa1b95dd804f9e282ec73f6e64321452a5397d4
Mrleoneed <mrleoneed@gmail.com>
2023-05-23 15:55:13 +0300

    Initial commit

---
+++ 01/script.js
@@ -0,0 +1,14 @@
+console.log("Hello")
+
+console.log("Артём" + " " + "Пидор")
+
+console.info("Hello")
+
+
+document.getElementById("out").innerHTML = "Артём пидор"
+document.getElementById("out").innerHTML = "Пидор"
+document.getElementById("out").innerHTML = "<b>Жирный шрифт</b>"
+
+
+document.querySelector(".header").innerHTML = "65";
+document.querySelector("#one").innerHTML = 777
