04d1abcb1187d7256ab7a16151b218f7f13a0766
Mrleoneed <mrleoneed@gmail.com>
2023-05-23 16:30:11 +0300

    unit 01

--- 01/script.js
+++ 01/script.js
@@ -12,3 +12,10 @@ document.getElementById("out").innerHTML = "<b>Жирный шрифт</b>"
 
 document.querySelector(".header").innerHTML = "65";
 document.querySelector("#one").innerHTML = 777
+
+
+let a = document.querySelector(".header")
+
+a.innerHTML = "suka artem"
+
+
