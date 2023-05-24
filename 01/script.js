369a789267046233a9390641694e882142c2c99e
Mrleoneed <mrleoneed@gmail.com>
2023-05-24 14:46:42 +0300

    unit 03

--- 01/script.js
+++ 01/script.js
@@ -1,9 +1,33 @@
-let inputIn = document.querySelector(".inputIn");
-let button = document.querySelector("button");
-let div = document.querySelector(".out")
+// let a = 5
+//
+// if (a != 9) {
+// 	console.log("Yes")
+// } else {
+// 	console.log("No")
+// }
+
+
+const button = document.querySelector("button");
+const input = document.querySelector(".age");
 
 button.onclick = () => {
-	let b = +inputIn.value
-	div.innerHTML = b;
-	inputIn.value = ""
+	let num = +input.value;
+	if (num >= 16 && num <= 60) {
+		console.log("Welcome")
+	} else if (num > 60) {
+		console.log("Older")
+	} else {
+		console.log("Bye")
+	}
+	switch (num) {
+		case 15:
+			console.log("потерпи")
+			break;
+		case 18:
+			console.log("можно пить алкоголь")
+			break;
+		default:
+			console.log("Okey")
+	}
 }
+
