3f162fc61448395c62de3f28f318b0413fa42da6
Mrleoneed <mrleoneed@gmail.com>
2023-05-26 14:01:19 +0300

    unit 07 10/10

--- 01/script.js
+++ 01/script.js
@@ -1,16 +1,46 @@
-let out = document.querySelector(".out")
+let f1 = document.querySelector(".f1")
 
-// for (let i = 0; i < 5; i++) {
-// 	for (let k = 0; k < 10; k++) {
-// 		out.innerHTML += "*"
-// 	}
-// 	out.innerHTML += '<br>'
-// }
-//
-
-for (let i = 1; i < 10; i++) {
-	for (let k = 1; k < 10; k++) {
-		out.innerHTML += `${i}*${k}=${i * k}<br>`
-	}
-	out.innerHTML += "<hr>"
+function one() {
+	console.log("work")
 }
+
+f1.onclick = () => {
+	console.log("Hello")
+}
+
+
+function two() {
+	console.log("work 2222")
+	return 54;
+}
+
+two()
+
+console.log(1 + two())
+
+let a = 9
+let b = 10
+
+function mult() {
+	console.log(a * b)
+	return a * b
+}
+
+mult()
+let c1 = mult()
+let c2 = 10 * mult()
+console.log(c1, c2)
+
+function mult2(x = 50, y = 50) {
+	return x * y
+}
+
+console.log(mult2(1, 2))
+
+document.querySelector(".f2").onclick = () => {
+	console.log("+++")
+}
+
+
+
+
