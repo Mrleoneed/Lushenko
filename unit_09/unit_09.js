d136114dbf8fdebcdf1d46963773b0cd9d791bdc
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 16:34:13 +0300

    unit 09 16/20

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -218,7 +218,11 @@ document.querySelector('.b-15').onclick = f15;
 // Добавьте кнопку .b-16, которая запускает функцию f16. Функция создает через createElement div c текстом 16 и добавляет ему класс bg-16. Созданный div добавляется в out-16 с помощью after.
 
 function f16() {
-
+	let out = document.querySelector(".out-16")
+	let a = document.createElement("div")
+	a.textContent = '16'
+	a.classList.add('bg-16')
+	out.after(a)
 }
 
 document.querySelector('.b-16').onclick = f16;
