e16e569bf3b4fbe29838686a4363dd1c2809cea2
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 15:52:37 +0300

    unit 09 5/20

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -47,6 +47,9 @@ document.querySelector('.b-4').onclick = f4;
 // По нажатию b-5 запускайте функцию f5, которая проверяет наличие класса bg-4 у блока out-4 (да, именно bg-4 у out-4 ). Результат - true или false, выводите в out-5.
 
 function f5() {
+	let out = document.querySelector(".out-5")
+	let out4 = document.querySelector(".out-4").classList.contains('bg-4')
+	out.innerHTML = out4
 
 }
 
