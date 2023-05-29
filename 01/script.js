1a4cffc9b24771b37532af1b9e61255afe254155
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 12:52:43 +0300

    unit 08 5/15

--- 01/script.js
+++ 01/script.js
@@ -1,46 +1,42 @@
-let f1 = document.querySelector(".f1")
-
-function one() {
-	console.log("work")
+for (let i = 0; i < 6; i++) {
+	console.log(i)
 }
 
-f1.onclick = () => {
-	console.log("Hello")
+let k = 0
+
+while (k < 5) {
+	console.log("k" + k)
+	k++
 }
 
-
-function two() {
-	console.log("work 2222")
-	return 54;
+let sum = 0;
+let p = 0;
+while (p <= 3) {
+	sum = sum + p;
+	p++;
 }
+console.log("sum = " + sum)
 
-two()
+// ****
+// ****
+// ****
+let out = document.querySelector(".out")
+p = 0;
+let outStr = " ";
+let flag = 3
 
-console.log(1 + two())
-
-let a = 9
-let b = 10
-
-function mult() {
-	console.log(a * b)
-	return a * b
+while (p < 4) {
+	let p1 = 0
+	while (p1 < 4) {
+		if (p1 < flag) {
+			outStr += '0'
+		} else {
+			outStr += "*";
+		}
+		p1++
+	}
+	flag--
+	outStr += '<br>'
+	p++
 }
-
-mult()
-let c1 = mult()
-let c2 = 10 * mult()
-console.log(c1, c2)
-
-function mult2(x = 50, y = 50) {
-	return x * y
-}
-
-console.log(mult2(1, 2))
-
-document.querySelector(".f2").onclick = () => {
-	console.log("+++")
-}
-
-
-
-
+out.innerHTML = outStr;
