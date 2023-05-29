20a26985f8f9ff6b6947b3d4e6c8d6d0f094f498
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 16:46:35 +0300

    unit 09 19/20

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -259,7 +259,12 @@ document.querySelector('.b-18').onclick = f18;
 // Добавьте кнопку .b-19, которая запускает функцию f19. Функция с помощью getAttribute получает data-b атрибут с параграфов p-19 и выводит в out-19 через пробел. Обратите внимание, что элементов p-19 больше одного.
 
 function f19() {
-
+	let out = document.querySelector(".out-19");
+	let p = document.querySelectorAll('.p-19');
+	for (let i = 0; i < p.length; i++) {
+		let data = p[i].getAttribute('data-b')
+		out.innerHTML += data + " "
+	}
 }
 
 document.querySelector('.b-19').onclick = f19;
