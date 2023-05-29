ceaca6121462c2d007daba9cf40a39efe157f49d
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 19:34:31 +0300

    unit 10 20/20

--- 01/script.js
+++ 01/script.js
@@ -1,31 +1,35 @@
-const one = document.querySelector(".one");
+let a = [1, 2, 3, 4]
+console.log(a)
 
-one.style.width = '150px'
+let t = a[0] // 1
+a[0] = a[a.length - 1]
+a[a.length - 1] = t
+console.log(a)
 
-one.classList.add('two', 'three')
-one.classList.remove('three')
-
-
-const toggle = document.querySelector(".toggle");
-
-toggle.onclick = function () {
-	this.classList.toggle('three')
-}
-
-// атрибуты
-
-console.log(one.getAttribute('data'))
-one.setAttribute('data-num', 6);
-
-
-let gas = document.querySelectorAll(".gas");
-for (let i = 0; i < gas.length; i++) {
-	gas[i].onclick = function () {
-		let gallons = document.querySelector(".gallons").value
-		let price = this.getAttribute("data")
-		console.log(gallons * price)
+let out = ''
+for (let i = 0; i < a.length; i++) {
+	if (a[i] % 2 === 0) {
+		out += a[i] + ' '
 	}
 }
+document.querySelector('.out-1').innerHTML = out
 
-let a = document.createElement('div')
-a.innerHTML = "Hello"
+
+let b = [4, 2, 5, 6, 7, 8, 44, 66, 77, 88, 9, 200, 1]
+let max = b[0]; //4
+
+for (let i = 0; i < b.length; i++) {
+	if (b[i] > max) {
+		max = b[i]
+	}
+
+}
+console.log(max)
+
+
+// sum
+
+let sum = 0;
+for (let i = 0; i < b.length; i++) {
+	sum = sum + b[i]
+}
