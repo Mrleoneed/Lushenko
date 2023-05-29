07f9e1ee534661f37e0e92dc434e42961a25f941
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 13:40:07 +0300

    unit 08 11/15

--- unit_08/unit_08.js
+++ unit_08/unit_08.js
@@ -229,6 +229,13 @@ document.querySelector('.b-10').onclick = t10;
 
 
 function t11() {
+	let out = document.querySelector(".out-11");
+	let divs = document.querySelectorAll(".div-11");
+	let k = 0;
+	while (k < divs.length) {
+		out.innerHTML += divs[k].innerHTML + " "
+		k++
+	}
 
 }
 
