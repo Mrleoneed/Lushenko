84a131c9a75a19118f69f33755631047f7012447
Mrleoneed <mrleoneed@gmail.com>
2023-05-29 15:35:49 +0300

    unit 09 3/20

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -25,7 +25,8 @@ document.querySelector('.b-2').onclick = f2;
 // По нажатию кнопки b-3 запускайте функцию f3, которая удаляет у блока out-3 класс .bg-3.
 
 function f3() {
-
+	let out = document.querySelector(".out-3")
+	out.classList.remove('bg-3')
 }
 
 document.querySelector('.b-3').onclick = f3;
