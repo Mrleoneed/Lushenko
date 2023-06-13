c56835a55aabf8a75e70630d53074a65c421328f
Mrleoneed <mrleoneed@gmail.com>
2023-06-13 13:54:32 +0300

    unit 17 10/10

--- unit_17/unit_17.js
+++ unit_17/unit_17.js
@@ -177,8 +177,10 @@ document.querySelector('.b-9').onclick = () => {
 let b10 = [[1, 2, 3], [3, 4, 6], [4, 5, 7], [8, 9, 3]]
 
 function t10() {
-
-
+	b10_res = b10.filter(e => {
+		return e.includes(3)
+	})
+	return b10_res
 }
 
 document.querySelector('.b-10').onclick = () => {
