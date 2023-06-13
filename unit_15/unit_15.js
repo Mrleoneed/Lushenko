b8b9d954f9a95cb243286c32ea2d0c732fcb4715
Mrleoneed <mrleoneed@gmail.com>
2023-06-13 10:41:40 +0300

    unit 15 02/13

--- unit_15/unit_15.js
+++ unit_15/unit_15.js
@@ -15,9 +15,14 @@ console.log(s1)
 // При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
 
 
-// let s2 =
+let s2 = new Set()
 
 const f2 = () => {
+	let input = document.querySelector(".i-2").value
+	s2.add(input)
+	console.log(s2)
+
+
 }
 
 document.querySelector('.b-2').onclick = f2;
