e3cbff0266cf0c3b2d52163b4e547fea5ebdd5a5
Mrleoneed <mrleoneed@gmail.com>
2023-06-13 13:25:07 +0300

    unit 15 03/10

--- unit_17/unit_17.js
+++ unit_17/unit_17.js
@@ -51,6 +51,10 @@ document.querySelector('.b-2').onclick = () => {
 let a3 = [4, "3", 6, 7, "12", 34, "56", 78, 90, 11];
 
 function t3() {
+	a3_res = a3.map(e => {
+		return parseInt(e)
+	})
+	return a3_res
 
 }
 
